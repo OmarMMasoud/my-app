34f0276d56f57c50e0398a666f9caaef42dfdd16
OmarMMasoud <omarm.masoud@outlook.com>
2023-04-05 18:17:17 +0300

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,74 @@
-import './App.css';
-import Search from './search';
+import React ,{useState} from 'react'
+import axios from 'axios'
+
 function App() {
 
-  const handelOnSearchChange = (searchData) =>{
-    console.log(searchData);
+  const [data,setData]= useState({})
+  const [location, setLocation] =useState({})
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '4402a4ec91mshe4a245e5d87b4d6p12fb83jsn8713a60a30bb',
+      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+    }
+  };
+  
+
+  const url =('https://weatherapi-com.p.rapidapi.com/current.json?q${location}', options)
+  
+
+  
+  const searchLocation =(event) =>{
+    if(event.key==="enter"){
+      axios.get(url).then((Response) =>{
+        setData(Response.data)
+        console.log(Response.data)
+      
+      })
+    }
   }
+    return (
 
-  return (
+      <div className="App">
+        <div className='search'>
+        <input type="text"
+        onKeyPress={(e) => searchLocation(e)}
+        onChange={event=> setLocation(event.target.value)} 
+        value= {location}
+        placeholder='write city name'/>  
+        </div>
+        <div className='container'>
 
-      <div className="container">
-        <h1>j</h1>
-      <Search OnSearchChange={handelOnSearchChange}/>
+          <div className='top'>
+
+            <div className='location'>
+              <p>Dallas</p>
+            </div>
+            <div className='temp'>
+              <h1>40°F</h1>              
+            </div>
+            <div className='description'>
+              <p>clouds</p>
+            </div>
+          </div>
+
+          <div className='bottom'>
+            <div className='feels'>
+             <p className='bold'> 65°F</p>
+             <p>Feels Like</p>
+            </div>
+            <div className='humidity'>
+             <p className='bold'>20%</p>
+             <p>humidity</p>
+            </div>
+            <div className='wind'>
+             <p>12 MPH</p>
+             <p>Wind Speed</p>
+            </div>
+          </div>
+
+        </div>
+
     </div>
   );
 }
